5c1984a3071085f318ffb9b34afea024fa1a1b9d
Noah2024 <Noah.Yurasko@tufts.edu>
2025-04-16 15:44:36 -0400

    HorizontalProbUpdated

--- script.js
+++ script.js
@@ -18,14 +18,17 @@ function addArrays(arr1, arr2) {//Github Copilot generated
     return arr1.map((val, index) => val + arr2[index]);
 }
 
-function sumArray(arr){
-    return arr.reduce((sum, val) => sum + val, 0);
+function sumStr(str){
+    let sum = 0;
+    str.split("").map((num) => sum += parseInt(num, 10));
+    return sum;
+    //arr.reduce((sum, val) => sum + val, 0); used for summingArrays
 }
 
-let statusArray = create2dArray(row, col);
+statusArray = Array.from({length: row}, () => "0".repeat(col));
 let probArray = create2dArray(row, col);
 
-//statusArray[0][5] = 1;
+statusArray[1] = "1111111000";
 console.log(statusArray, probArray);
 
 function updateHorizontalProb() {
@@ -34,19 +37,30 @@ function updateHorizontalProb() {
         const curtShipLen = shipsLen[ship].length;
         console.log(curtShipArr);
         for (let i = 0; i < row; i +=1){
-            for (let j = 0; j+curtShipLen <= col; j += 1){
-                if (sumArray(statusArray.slice(j, j+curtShipLen)) != 0){//Checks if there are any ones in the status array
+            for (let j = 0; j+curtShipLen <= col; j += 1){//sumArray(statusArray.slice(j, j+curtShipLen)
+                if (sumStr(statusArray[i].slice(j, j+curtShipLen)) == 0){//Checks if there are any ones in the status array
                     const endOffset = col-(j+curtShipLen) //The offset from the front added to allow the addition of the arrays
                     const startOffset  = j //The offset from the back added to allow the addition of the arrays
                     let addlArray = "0".repeat(startOffset) + curtShipArr + "0".repeat(endOffset);
                     addlArray = addlArray.split("").map(Number);//Convert string to array
                     probArray[i] = addArrays(probArray[i], addlArray);
                 }
-                console.log(probArray[i]);
             }
         }
     }
     console.log(probArray);
 }
 
-updateHorizontalProb();
+function updateVerticalProb() {
+    for (const ship of shipsLeft){
+        const curtShipArr = shipsLen[ship];
+        const curtShipLen = shipsLen[ship].length;
+        console.log(curtShipArr);
+        for (let i = 0; i < col; i +=1){
+            const verticalSlice = statusArray.slice(i, i+curtShipLen)
+        }
+    console.log(probArray);
+    }
+}
+
+updateHorizontalProb()
