6d7e855093dc7bd062d1df3570bb045a5f96cea0
Noah2024 <Noah.Yurasko@tufts.edu>
2025-04-20 23:40:23 -0400

    FixedLogicError(MadeOOP)

--- script.js
+++ script.js
@@ -1,106 +1,121 @@
-
-const row = 10;
-const col = 10;
-const shipsLen = {"carrier": "11111", "battleship": "1111", "crusier": "111", "submarine": "111", "destroyer": "11"};
-const shipsLeft = ["carrier", "battleship", "crusier", "submarine", "destroyer"]
-
-function create2dArray(row, col){
-    array = Array.from({"length": row}, () => 
-        Array.from({"length": col}, () => 0)
-    );
-    return array;
-}
-
-function createArrays(){
-    let allChanceArray = create2dArray(row, col);
-    let probArray = create2dArray(row, col);
-}
-
-function addArrays(arr1, arr2) {//Github Copilot generated
-    if (arr1.length !== arr2.length) {
-        throw new Error("Arrays must have the same length");
+class gameSpace {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+        this.shipsLen = {
+            "carrier": "11111",
+            "battleship": "1111",
+            "crusier": "111",
+            "submarine": "111",
+            "destroyer": "11"
+        };
+        this.shipsLeft = ["carrier", "battleship", "crusier", "submarine", "destroyer"];
+        this.statusArray = Array.from({ length: row }, () => "0".repeat(col));
+        this.numChanceArray = this.create2dArray(row, col);
+        this.probArray = this.create2dArray(row, col);
+        this.updateHorizontalProb();
+        this.updateVerticalProb();
+        this.updateProbArray();
     }
-    return arr1.map((val, index) => val + arr2[index]);
-}
 
-function sumStr(str){
-    let sum = 0;
-    str.split("").map((num) => sum += parseInt(num, 10));
-    return sum;
-    //arr.reduce((sum, val) => sum + val, 0); used for summingArrays
-}
+    create2dArray(row, col) {
+        return Array.from({ length: row }, () => Array.from({ length: col }, () => 0));
+    }
 
-function updateHorizontalProb() {
-    for (const ship of shipsLeft){
-        const curtShipArr = shipsLen[ship];
-        const curtShipLen = shipsLen[ship].length;
-
-        for (let i = 0; i < row; i +=1){
-
-            for (let j = 0; j+curtShipLen <= col; j += 1){//sumArray(statusArray.slice(j, j+curtShipLen)
-                if (sumStr(statusArray[i].slice(j, j+curtShipLen)) == 0){//Checks if there are any ones in the status array
-                    const endOffset = col-(j+curtShipLen) //The offset from the front added to allow the addition of the arrays
-                    const startOffset  = j //The offset from the back added to allow the addition of the arrays
-                    let addlArray = "0".repeat(startOffset) + curtShipArr + "0".repeat(endOffset);
-                    addlArray = addlArray.split("").map(Number);//Convert string to array
-                    allChanceArray[i] = addArrays(allChanceArray[i], addlArray);
-                }
-            }
+    addArrays(arr1, arr2) {
+        if (arr1.length !== arr2.length) {
+            throw new Error("Arrays must have the same length");
         }
+        return arr1.map((val, index) => val + arr2[index]);
     }
-}
 
-function updateVerticalProb() {
-    for (const ship of shipsLeft){
-        const curtShipArr = shipsLen[ship];
-        const curtShipLen = shipsLen[ship].length;
+    sumStr(str) {
+        let sum = 0;
+        str.split("").map((num) => sum += parseInt(num, 10));
+        return sum;
+    }
 
-        for (let i = 0; i < col; i +=1){
-            let verticalSlice = "";
-            statusArray.map((val) => verticalSlice += val[i]);
+    zeroProbArrays(){
+        this.numChanceArray = this.create2dArray(this.row, this.col);
+        this.probArray = this.create2dArray(this.row, this.col);
+    }
 
-            for (let j = 0; j+curtShipLen <= row; j += 1){
-                if (sumStr(verticalSlice.slice(j, j+curtShipLen)) == 0){//Checks if there are any ones in the status array
-                    const endOffset = row-(j+curtShipLen) //The offset from the front added to allow the addition of the arrays
-                    const startOffset  = j //The offset from the back added to allow the addition of the arrays
-                    let addlArray = "0".repeat(startOffset) + curtShipArr + "0".repeat(endOffset);
-                    addlArray = addlArray.split("").map(Number);//Convert string to array
-                    allChanceArray[i] = addArrays(allChanceArray[i], addlArray);
-                    
-                    for (let k = 0; k < row; k++) {//updatig prob array colum wise
-                        allChanceArray[k][i] += addlArray[k];
+    updateHorizontalProb() {
+        for (const ship of this.shipsLeft) {
+            const curtShipArr = this.shipsLen[ship];
+            const curtShipLen = this.shipsLen[ship].length;
+            for (let i = 0; i < this.row; i += 1) {
+                for (let j = 0; j + curtShipLen <= this.col; j += 1) {
+                    if (this.sumStr(this.statusArray[i].slice(j, j + curtShipLen)) == 0) {
+                        const endOffset = this.col - (j + curtShipLen);
+                        const startOffset = j;
+                        let addlArray = "0".repeat(startOffset) + curtShipArr + "0".repeat(endOffset);
+                        addlArray = addlArray.split("").map(Number);
+                        this.numChanceArray[i] = this.addArrays(this.numChanceArray[i], addlArray);
                     }
-                    
                 }
             }
         }
     }
-}
 
-function updateProbArray(){
-    sum = 0;
-    for (let i = 0; i < row; i += 1){
-        allChanceArray[i].map((val) => sum += val);
+    updateVerticalProb() {
+        for (const ship of this.shipsLeft) {
+            const curtShipArr = this.shipsLen[ship];
+            const curtShipLen = this.shipsLen[ship].length;
+
+            for (let i = 0; i < this.col; i += 1) {
+                let verticalSlice = "";
+                this.statusArray.map((val) => verticalSlice += val[i]);
+
+                for (let j = 0; j + curtShipLen <= this.row; j += 1) {
+                    if (this.sumStr(verticalSlice.slice(j, j + curtShipLen)) == 0) {
+                        const endOffset = this.row - (j + curtShipLen);
+                        const startOffset = j;
+                        let addlArray = "0".repeat(startOffset) + curtShipArr + "0".repeat(endOffset);
+                        addlArray = addlArray.split("").map(Number);
+
+                        for (let k = 0; k < this.row; k++) {
+                            this.numChanceArray[k][i] += addlArray[k];
+                        }
+                    }
+                }
+            }
+        }
     }
-    for (let i = 0; i < row; i += 1){
-        allChanceArray[i].map((val, index) => probArray[i][index] = val/sum);
+
+    updateProbArray() {
+        let sum = 0;
+        for (let i = 0; i < this.row; i += 1) {
+            this.numChanceArray[i].map((val) => sum += val);
+        }
+        for (let i = 0; i < this.row; i += 1) {
+            this.numChanceArray[i].map((val, index) => this.probArray[i][index] = val / sum);
+        }
+    }
+
+    setStatusArray(row, col, value) {
+        this.zeroProbArrays();
+        this.statusArray[row] = this.statusArray[row].substring(0, col) + value + this.statusArray[row].substring(col + 1);
+        this.updateHorizontalProb();
+        this.updateVerticalProb();
+        this.updateProbArray();
+    }
+
+    getStatusArray() {
+        return this.statusArray;
+    }
+
+    getNumChanceArray() {
+        return this.numChanceArray;
+    }
+
+    getProbArray() {
+        return this.probArray;
     }
 }
 
-function setStatusArray(row, col, value){
-    statusArray[row] = statusArray[row].substring(0, col) + value + statusArray[row].substring(col + 1);
-    updateHorizontalProb();
-    updateVerticalProb();
-    updateProbArray();
-}
-
-function getStatusArray(){
-    return statusArray;
-}
-
-//exmpleUsage
-//setStatusArray(1, 0, "1");
-//setStatusArray(9, 1, "1");
-//spdateHorizontalProb();
-//spdateVerticalProb();
-//spdateProbArray();
+// Example Usage
+// const game = new gameSpace(10, 10);
+// console.log(game.getProbArray());
+// game.setStatusArray(0, 0, "1");
+// console.log(game.getProbArray());
